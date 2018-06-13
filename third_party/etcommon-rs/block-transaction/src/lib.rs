c7906a367ed0ce7caa8cccda7e9c4834f5e1ba0c
David Phan <davidphan33@gmail.com>
2018-06-13 10:55:23 +0100

    added etcommon-rs/block-transaction

---
+++ third_party/etcommon-rs/block-transaction/src/lib.rs
@@ -0,0 +1,23 @@
+#![no_std]
+
+#[macro_use]
+extern crate sgx_tstd as std;
+
+extern crate bigint;
+extern crate rlp;
+extern crate secp256k1;
+extern crate sha3;
+extern crate block_core;
+
+mod transaction;
+mod address;
+
+pub use block_core::*;
+pub use transaction::*;
+pub use address::FromKey;
+
+use bigint::H256;
+
+pub trait RlpHash {
+    fn rlp_hash(&self) -> H256;
+}
